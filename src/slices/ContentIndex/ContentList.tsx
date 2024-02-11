98e1d7d505278a71181eb0f0b785f66dab48ec08
TURP1 <turpicrypto@gmail.com>
2024-02-12 00:17:17 +0200

    add hover animation on Project

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -3,7 +3,8 @@
 import {asImageSrc, Content, isFilled} from "@prismicio/client";
 import Link from "next/link";
 import {MdArrowOutward} from "react-icons/md";
-import {useRef, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
+import {gsap} from 'gsap';
 
 
 type ContentListProps = {
@@ -21,8 +22,47 @@ export default function ContentList({
                                     }: ContentListProps) {
     const [currentItem, setCurrentItem] = useState<null | number>(null);
     const component = useRef(null);
+    const revealRef = useRef(null);
+
+    const lastMousePosition = useRef({x: 0, y: 0});
 
     const urlPrefix = contentType === 'Blog' ? '/blog' : '/project';
+
+    useEffect(() => {
+        const handleMouseMove = (e: MouseEvent) => {
+            const mousePosition = {x: e.clientX, y: e.clientY + window.scrollY}
+
+            const speed = Math.sqrt(Math.pow(mousePosition.x - lastMousePosition.current.x, 2))
+
+            let ctx = gsap.context(() => {
+                const maxX = window.innerWidth - 220
+                const maxY = window.scrollY + window.innerHeight - 350
+                if (currentItem !== null) {
+
+                    gsap.to(revealRef.current, {
+                        x: gsap.utils.clamp(0, maxX, mousePosition.x - 110),
+                        y: gsap.utils.clamp(0, maxY, mousePosition.y - 160),
+                        rotation: speed * (mousePosition.x > lastMousePosition.current.x ? 1 : -1),
+                        ease: 'back.out',
+                        opacity: 1,
+                        duration: 1.3,
+                    })
+
+                    lastMousePosition.current = mousePosition;
+                }
+                return () => ctx.revert()
+            }, component)
+        }
+
+
+        window.addEventListener('mousemove', handleMouseMove)
+
+        return () => {
+            window.removeEventListener('mousemove', handleMouseMove)
+        }
+    }, [currentItem]);
+
+
     const contentImages = items.map((item) => {
         const image = isFilled.image(item.data.hover_image) ? item.data.hover_image : fallBackItemImage
 
@@ -35,7 +75,7 @@ export default function ContentList({
         })
     })
 
-    const onMouseEnter = (index : number) => {
+    const onMouseEnter = (index: number) => {
         setCurrentItem(index)
     }
     const onMouseLeave = () => {
@@ -45,9 +85,9 @@ export default function ContentList({
     return (
         <div ref={component}>
             <ul className="grid border-b border-b-slate-100"
-            onMouseLeave={onMouseLeave}>
+                onMouseLeave={onMouseLeave}>
                 {items.map((item, index) => (
-                    <>
+                    <React.Fragment key={index}>
                         {
                             isFilled.keyText(item.data.title) && (
                                 <li key={index}
@@ -74,16 +114,16 @@ export default function ContentList({
                                 </li>
                             )
                         }
-                    </>
+                    </React.Fragment>
                 ))}
                 {/* Hover element */}
                 <div
-                    className="hover-reveal pointer-events-none absolute left-0 top-0 -z-10 h-[320px] w-[220px] rounded-lg bg-cover bg-center opacity-0f transition-[background] duration-300"
+                    className="hover-reveal pointer-events-none absolute left-0 top-0 -z-10 h-[320px] w-[220px] rounded-lg bg-cover bg-center opacity-0 transition-[background] duration-300"
                     style={{
                         backgroundImage:
                             currentItem !== null ? `url(${contentImages[currentItem]})` : "",
                     }}
-                    // ref={revealRef}
+                    ref={revealRef}
                 ></div>
             </ul>
 
