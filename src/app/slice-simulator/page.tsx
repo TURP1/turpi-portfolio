47bd7448f71537475843a75b0f2eadba6039eea5
TURP1 <turpicrypto@gmail.com>
2024-02-12 14:39:12 +0200

    add slice simulator bg

--- src/app/slice-simulator/page.tsx
+++ src/app/slice-simulator/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import { SliceSimulator } from "@slicemachine/adapter-next/simulator";
-import { SliceZone } from "@prismicio/react";
+import {SliceSimulator} from "@slicemachine/adapter-next/simulator";
+import {SliceZone} from "@prismicio/react";
 
-import { components } from "../../slices";
+import {components} from "../../slices";
 
 export default function SliceSimulatorPage() {
-  return (
-    <SliceSimulator
-      sliceZone={(props) => <SliceZone {...props} components={components} />}
-    />
-  );
+    return (
+        <SliceSimulator
+            background='#30162c'
+            sliceZone={(props) => <SliceZone {...props} components={components}/>}
+        />
+    );
 }
