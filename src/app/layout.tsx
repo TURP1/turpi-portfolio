4f70abf893c6f2b5f139ef46f0724a280863d3ca
TURP1 <turpicrypto@gmail.com>
2024-02-12 11:37:50 +0200

    add nextCodemod

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import {Urbanist} from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import clsx from "clsx";
 
 const urbanist = Urbanist({subsets: ["latin"]});
